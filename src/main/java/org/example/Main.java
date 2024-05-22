28a94979e3654fabc454dec4bad6d75a800634e3
HissDev <hissoffc@gmail.com>
2024-05-22 23:44:48 +0300

    Начал изучать что такое коллекции. Изучил что такое ArrayList и начал изучать LinkedList.
    Изучил что такое сам LinkedList а также стек и очередь. Также в принципе как работать с LinkedList.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,32 +1,15 @@
 package org.example;
 
-import org.example.generics.List;
-import org.example.generics.weapon.*;
-import org.example.taskOop.*;
+import org.example.collection.TestCollection;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
 
-        Mage<Wand> mage = new Mage<>("Hiss", 20);
-        mage.setWeapon(new Wand());
+        TestCollection testCollection = new TestCollection();
 
-        Archer<Bow> archer = new Archer<>("Arina", 10);
-        archer.setWeapon(new Bow());
+        testCollection.getCollectionLinkedList();
 
-        Warrior<Sword> warrior = new Warrior<>("Ivan", 13);
-        warrior.setWeapon(new Sword());
-
-        printWeaponDamage(mage);
-
-    }
-
-    /*
-    * Для того, чтобы принять какой-либо тип оружия нам нужно
-    * Создать дженерик который принимает типы наследованные от оружия,
-    * то есть параметризация на уровне метода
-    * И задать героя который параметризован тем типом оружия, который наседован у метода
-    * */
-    public static <T extends Weapon> void printWeaponDamage(Hero<T> hero) {
-        System.out.println(hero.getWeapon().getDamage());
     }
 }
---
+++ src/main/java/org/example/collection/TestCollection.java
@@ -0,0 +1,85 @@
+package org.example.collection;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class TestCollection {
+
+    public List<Integer> getCollectionArrayList() {
+        List<Integer> integers = new ArrayList<>();
+
+        integers.add(1);
+        integers.add(2);
+        integers.add(3);
+        integers.add(4);
+        integers.add(5);
+
+        return integers;
+    }
+
+    /*
+     * LinkedList - это реализация двусвязного списка,
+     * который входит в коллекции из стандартной библиотеки Java (Java Collections Framework).
+     * В отличие от массивов и списков, основанных на массивах (например, ArrayList), LinkedList состоит из узлов,
+     * где каждый узел содержит ссылку на предыдущий и следующий элементы списка, а также данные.
+     *
+     * Основные характеристики LinkedList:
+     * 1. Динамическая структура данных: LinkedList может динамически изменять свой размер, добавляя или удаляя элементы.
+     * 2. Эффективность операций вставки и удаления: Вставка и удаление элементов в середину или начало списка более эффективны по сравнению с ArrayList,
+     * так как не требуется сдвиг элементов.
+     * 3. Доступ к элементам: Доступ к элементам по индексу менее эффективен, чем у ArrayList,
+     * так как для этого требуется пройти по ссылкам от начала списка до нужного элемента.
+     * */
+    public void getCollectionLinkedList() {
+        LinkedList<String> linkedList = new LinkedList<>();
+
+//        Пример использования
+        linkedList.add("One");
+        linkedList.add("Two");
+        linkedList.add("Three");
+
+        // Добавляем элемент в начало
+        linkedList.addFirst("New one");
+        // Добавляем элемент в конец
+        linkedList.addLast("New three");
+
+        // Получаем первый и последний элемент
+        System.out.println("First element: " + linkedList.getFirst());
+        System.out.println("Last element: " + linkedList.getLast());
+
+//        Удаляем элементы
+        linkedList.removeFirst();
+        linkedList.removeLast();
+
+        for (String item : linkedList) {
+            System.out.println(item);
+        }
+        System.out.println();
+
+        // Пример реализации стека (первый пришёл - последниё ушёл)
+        LinkedList<String> stack = new LinkedList<>();
+        stack.push("One");
+        stack.push("Two");
+        stack.push("Three");
+
+        System.out.println("LIFO: " + stack.pop());
+        System.out.println("LIFO: " + stack.pop());
+        System.out.println("LIFO: " + stack.pop());
+
+        System.out.println();
+
+//        Пример реализации очереди (первый пришёл - первый ушёл)
+        LinkedList<String> queue = new LinkedList<>();
+        queue.offer("One");
+        queue.offer("Two");
+        queue.offer("Three");
+
+        System.out.println("FIFO: " + queue.poll());
+        System.out.println("FIFO: " + queue.poll());
+        System.out.println("FIFO: " + queue.poll());
+
+    }
+
+
+}
