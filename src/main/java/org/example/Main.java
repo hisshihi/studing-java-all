b3b2a1dc88ca186b540fe5448807cdb09f31b28e
HissDev <hissoffc@gmail.com>
2024-05-24 02:39:41 +0300

    Изучил методы equals and hashCode. Переопределил эти методы, чтобы подстроить логику под себя, а именно проверять методы не по области памяти, а по значению.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,18 @@
 package org.example;
 
-import org.example.collection.TestCollection;
-
-import java.util.List;
+import org.example.person.Person;
 
 public class Main {
     public static void main(String[] args) {
 
-        TestCollection testCollection = new TestCollection();
+        Person denis = new Person(1, "Denis", "Hiss");
+        Person arina = new Person(2, "Arina", "Kovaleva");
+        Person denis2 = new Person(1, "Denis", "Hiss");
 
-        testCollection.listExample();
+        System.out.println(denis.hashCode());
+        System.out.println(arina.hashCode());
+        System.out.println(denis2.hashCode());
+        System.out.println(denis.equals(denis2));
 
     }
 }
