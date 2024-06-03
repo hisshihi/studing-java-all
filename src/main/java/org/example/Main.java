64b193e6230ef6a278d16810a7a4cf423b28de03
HissDev <hissoffc@gmail.com>
2024-06-03 19:29:36 +0300

    Изучил что такое состояние потоков. Есть несколько состояний потоков, такие как NEW, RUNNABLE, TERMINATED, WAIT и так далее.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,13 @@ package org.example;
 
 import org.example.threadDemo.SimpleRunnable;
 import org.example.threadDemo.SimpleThread;
+
 public class Main {
     public static void main(String[] args) {
 
         SimpleThread simpleThread = new SimpleThread();
+//        Состояние NEW
+        System.out.println(simpleThread.getName() + ": " + simpleThread.getState());
 
         SimpleRunnable simpleRunnable = new SimpleRunnable();
         Thread runnbleThread = new Thread(new SimpleRunnable());
@@ -15,11 +18,18 @@ public class Main {
         simpleThread.start();
         runnbleThread.start();
         lambdaThread.start();
+
+        //        Состояние RUNNABLE
+        System.out.println(simpleThread.getName() + ": " + simpleThread.getState());
+
 //        Эта строка обозначает, что этот поток будет ждать, пока simpleThread не выполнит свою работу
         try {
             simpleThread.join();
             runnbleThread.join();
             lambdaThread.join();
+
+            //        Состояние TERMINATED
+            System.out.println(simpleThread.getName() + ": " + simpleThread.getState());
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
