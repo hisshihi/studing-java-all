854e1fb511aa23241a8e49c494d5912bea4c7bab
HissDev <hissoffc@gmail.com>
2024-06-03 19:20:10 +0300

    Второй способ создания потоков. С помощью интерфейса Runnable

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,25 @@
 package org.example;
 
+import org.example.threadDemo.SimpleRunnable;
 import org.example.threadDemo.SimpleThread;
 public class Main {
     public static void main(String[] args) {
 
         SimpleThread simpleThread = new SimpleThread();
+
+        SimpleRunnable simpleRunnable = new SimpleRunnable();
+        Thread runnbleThread = new Thread(new SimpleRunnable());
+        Thread lambdaThread = new Thread(() -> System.out.println("Hello from lambda: " + Thread.currentThread().getName()));
+
 //        Запуск потока
         simpleThread.start();
+        runnbleThread.start();
+        lambdaThread.start();
 //        Эта строка обозначает, что этот поток будет ждать, пока simpleThread не выполнит свою работу
         try {
             simpleThread.join();
+            runnbleThread.join();
+            lambdaThread.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
