232b1663a459f413797fe58c9cd5b034770cf89e
HissDev <hissoffc@gmail.com>
2024-06-22 14:55:10 +0300

    Создал docker-compose.yml для запуска базы данных, чтобы изучать sql

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,9 @@
 package org.example;
 
-import org.example.reflectionApiTasks.Car;
-import org.example.reflectionApiTasks.ReflectionApiTasks;
-
 public class Main {
     public static void main(String[] args) {
 
-        Car car = new Car("Toyota", "Corolla");
-        ReflectionApiTasks apiTasks = new ReflectionApiTasks();
-        System.out.println(apiTasks.task1(car));
+
 
     }
 }
