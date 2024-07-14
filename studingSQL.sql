5aefc660485a5882aa5fb8cb64cf398f8a7fe272
HissDev <hissoffc@gmail.com>
2024-07-14 13:57:19 +0300

    Более подробно разобрал, что такое внешние ключи.

--- studingSQL.sql
+++ studingSQL.sql
@@ -13,18 +13,18 @@ CREATE TABLE company
 --     Описываем структуру таблицы
     id   INT PRIMARY KEY,
     name VARCHAR(128) UNIQUE NOT NULL,
-    date DATE                NOT NULL CHECK ( date > '2000-01-01' AND date < '2024-07-03'),
--- Также можно указывать ограничения сразу по нескольким полям
-    PRIMARY KEY (id),
+    date DATE                NOT NULL CHECK ( date > '2000-01-01' AND date < '2024-07-03')
+    -- Также можно указывать ограничения сразу по нескольким полям
+--     PRIMARY KEY (id),
     -- Не можем вставить компания с одним и тем же названием и годом
-    UNIQUE (name, date)
+--     UNIQUE (name, date)
 
 -- NOT NULL - не может быть пустым
 -- UNIQUE - поле должно быть уникальным
 -- CHECK - ограничение к примеру по дате или кол-ву
 -- PRIMARY KEY == UNIQUE NOT NULL - обозначает, что поле теперь первичный ключ
 -- FOREIGN KEY - внешний ключ
-);
+    );
 
 -- Удаление таблицы
 DROP TABLE public.company;
@@ -32,7 +32,7 @@ DROP TABLE public.company;
 -- Добавление записи в таблицу
 INSERT INTO company(id, name, date)
 VALUES (1, 'HissInc', '2024-07-02'),
-       (2, 'Apple', '2002-10-18'),
+       (2, 'Nvidia', '2002-10-18'),
        (3, 'OpenAI', '2013-08-10');
 
 -- DROP TABLE employee;
@@ -42,15 +42,20 @@ CREATE TABLE employee
     id         SERIAL PRIMARY KEY,
     first_name VARCHAR(128) NOT NULL,
     last_name  VARCHAR(128) NOT NULL,
+    company_id INT REFERENCES company (id),
     salary     INT,
     UNIQUE (first_name, last_name)
+--     FOREIGN KEY (company_id) REFERENCES company
 );
 
+DROP TABLE employee;
 
-INSERT INTO employee(first_name, last_name, salary)
-VALUES ('Hiss', 'Dev', 10000),
-       ('Arina', 'Kovaleva', 9000),
-       ('Sergey', 'Ushakov', 7000);
+INSERT INTO employee(first_name, last_name, salary, company_id)
+VALUES ('Hiss', 'Dev', 10000, 1),
+       ('Arina', 'Kovaleva', 9000, 1),
+       ('Sergey', 'Ushakov', 7000, 3),
+       ('Nikita', 'Gluharev', 6000, 2),
+       ('Danila', 'Lebedev', 9000, NULL);
 
 -- Выборка данных
 -- DISTINCT - используется для выборки уникальных значений
