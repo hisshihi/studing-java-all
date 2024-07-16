5a256c35f32a0e33f89a088cfcbd66697ff98e5a
HissDev <hissoffc@gmail.com>
2024-07-16 16:20:23 +0300

    Изучил как работать с LEFT, RIGHT, CROSS and FULL JOIN. Как работает каждый из них и в чём отличие. Вкратце: 1. LEFT JOIN - выводит все записи таблицы находящейся с лева. Т.е. если мы объединяем компании и их сотрудников, то выведутся все компании. 2. RIGHT JOIN - то же самое, только будет выведены все записи правой таблицы. 3. FULL JOIN - будут выведены все записи. 4. CROSS JOIN - само по себе не требует объединения, т.к. является объединением по умолчанию, выводя все требуемые записи из используемых таблиц.

--- studingJavaMiddleLvl.sql
+++ studingJavaMiddleLvl.sql
@@ -11,14 +11,14 @@ INNER JOIN или же JOIN
 SELECT c.name,
        employee.first_name || ' ' || employee.last_name fio,
        ec.contact_id,
-       c2.number || ' ' || c2.type contact
+       c2.number || ' ' || c2.type                      contact
 FROM employee
-JOIN public.company c
-    ON c.id = employee.company_id
-JOIN employee_contact ec
-    ON employee.id = ec.employee_id
-JOIN contact c2
-ON ec.contact_id = c2.id;
+         JOIN public.company c
+              ON c.id = employee.company_id
+         JOIN employee_contact ec
+              ON employee.id = ec.employee_id
+         JOIN contact c2
+              ON ec.contact_id = c2.id;
 
 -- CROSS JOIN
 /*
@@ -26,10 +26,51 @@ ON ec.contact_id = c2.id;
 */
 SELECT *
 FROM company
-CROSS JOIN (select count(*) FROM  employee) t;
+         CROSS JOIN (select count(*) FROM employee) t;
 
--- INNER JOIN -> JOIN
--- CROSS JOIN
--- LEFT JOIN
--- RIGHT JOIN
--- FULL JOIN
+/*
+Для того, чтобы пройтись с помощью LEFT JOIN
+Нам нужно определить, от какой таблицы будем идти
+Потому что результирующий набор таблицы от которой будем идти
+дудет содержать все записи этой таблицы
+
+Т.е. будут отображены все записи слева от LEFT JOIN
+*/
+SELECT c.name,
+       e.first_name || ' ' || e.last_name fio
+FROM company c
+         LEFT JOIN employee e
+                   ON c.id = e.company_id;
+
+-- Как допустим тут, не будет отображена компания, к которой не прикреплён сотрудник
+SELECT c.name,
+       e.first_name
+FROM employee e
+         LEFT JOIN company c ON e.company_id = c.id;
+
+-- RIGHT JOIN делает то же самое, но отображается справа
+SELECT c.name,
+       e.first_name || ' ' || e.last_name fio
+FROM company c
+         RIGHT JOIN employee e
+                    ON c.id = e.company_id
+                        AND c.date > '2012-01-01';
+
+/*
+FULL JOIN
+То же самое, но объединяет RIGHT AND LEFT и отображает все записи
+*/
+SELECT c.name,
+       e.first_name || ' ' || e.last_name fio
+FROM company c
+         FULL JOIN employee e
+                   ON c.id = e.company_id;
+
+/*
+CROSS JOIN
+Не требудет объединение таблиц, потому что сам по себе объединяет и выводит все записи
+*/
+SELECT c.name,
+       e.first_name
+FROM company c
+         CROSS JOIN employee e;
